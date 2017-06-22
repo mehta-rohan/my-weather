cd25b1a334c6f2ac11d14227067d9313ff53bf99
unknown <rohanmehta0077@gmail.com>
2017-06-23 01:25:55 +0530

    test sync

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var weather = require('./router/weather'); 
 var pushNotification = require('./router/pushNotification');
-var surprise = require('./router/surprise');
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -15,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger('dev'));
 app.use('/weather',weather);
 app.use('/push',pushNotification);
-app.use('/surprise',surprise);
+
 
 //server start
 app.listen(port,() => console.log(`Server listening @ ${port}`));
