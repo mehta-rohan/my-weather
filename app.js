78ee276a084835e3c363c098f53d3fa1acb6df44
unknown <rohanmehta0077@gmail.com>
2017-06-23 00:57:56 +0530

    test delete

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var weather = require('./router/weather'); 
 var pushNotification = require('./router/pushNotification');
+var surprise = require('./router/surprise');
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -14,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger('dev'));
 app.use('/weather',weather);
 app.use('/push',pushNotification);
-
+app.use('/surprise',surprise);
 
 //server start
 app.listen(port,() => console.log(`Server listening @ ${port}`));
