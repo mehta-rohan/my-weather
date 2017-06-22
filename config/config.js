c2753ba9ddb792d7df00d036f278113a6b770244
unknown <rohanmehta0077@gmail.com>
2017-06-22 22:39:11 +0530

    adding configurations

---
+++ config/config.js
@@ -0,0 +1,12 @@
+var googleAPI = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+var weatherAPI = "https://api.darksky.net/forecast/";
+var weatherAPIKey = "c82b3f0ebc92478f41d03f1cdc8a073a/";
+var serviceAccount = "firebase-adminsdk-awhxg@reminder-65597.iam.gserviceaccount.com";
+
+
+module.exports = {
+	googleAPI,
+	weatherAPI,
+	weatherAPIKey,
+	serviceAccount
+}
